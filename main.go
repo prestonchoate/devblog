d59090c3d01cee32b96e99cc756efa1582a54080
Preston Choate <pchoate15@gmail.com>
2024-01-20 23:06:47 -0500

    Restructured some templates, added project listing and project view templates

--- main.go
+++ main.go
@@ -21,17 +21,29 @@ type Post struct {
 }
 
 type Project struct {
-	ID          int
+	Id          int
 	Title       string
 	Image       string
 	Description string
 	Link        string
 }
 
+type Link struct {
+	Title string
+	URL   string
+}
+
 var templates *template.Template
 var templateDirs = []string{
 	"templates/partials/*.html",
 	"templates/*.html",
+}
+
+var links = []Link{
+	{Title: "Home", URL: "/"},
+	{Title: "Blog", URL: "/blog"},
+	{Title: "Projects", URL: "/projects"},
+	{Title: "About", URL: "/about"},
 }
 
 var posts = []Post{
@@ -45,11 +57,11 @@ var posts = []Post{
 }
 
 var projects = []Project{
-	{ID: 1, Title: "Project 1", Image: "/public/placeholder.svg", Description: "Description 1", Link: "https://github.com/prestonchoate"},
-	{ID: 2, Title: "Project 2", Image: "/public/placeholder.svg", Description: "Description 2", Link: "https://github.com/prestonchoate"},
-	{ID: 3, Title: "Project 3", Image: "/public/placeholder.svg", Description: "Description 3", Link: "https://github.com/prestonchoate"},
-	{ID: 4, Title: "Project 4", Image: "/public/placeholder.svg", Description: "Description 4", Link: "https://github.com/prestonchoate"},
-	{ID: 5, Title: "Project 5", Image: "/public/placeholder.svg", Description: "Description 5", Link: "https://github.com/prestonchoate"},
+	{Id: 1, Title: "Project 1", Image: "/public/placeholder.svg", Description: "Description 1", Link: "https://github.com/prestonchoate"},
+	{Id: 2, Title: "Project 2", Image: "/public/placeholder.svg", Description: "Description 2", Link: "https://github.com/prestonchoate"},
+	{Id: 3, Title: "Project 3", Image: "/public/placeholder.svg", Description: "Description 3", Link: "https://github.com/prestonchoate"},
+	{Id: 4, Title: "Project 4", Image: "/public/placeholder.svg", Description: "Description 4", Link: "https://github.com/prestonchoate"},
+	{Id: 5, Title: "Project 5", Image: "/public/placeholder.svg", Description: "Description 5", Link: "https://github.com/prestonchoate"},
 }
 
 func loadTemplates() (templates *template.Template) {
@@ -69,12 +81,13 @@ func init() {
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("homeHandler")
 	// only send the first 3 posts and projects to the home page
 	data := map[string]interface{}{
-		"title":    "Home",
-		"posts":    posts[:3],
-		"projects": projects[:3],
+		"title":      "Home",
+		"posts":      posts[:3],
+		"projects":   projects[:3],
+		"links":      links,
+		"showHeader": true,
 	}
 	err := templates.ExecuteTemplate(w, "home_page", data)
 	if err != nil {
@@ -84,10 +97,10 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func listPosts(w http.ResponseWriter, r *http.Request) {
-	log.Println("listPosts")
 	data := map[string]interface{}{
 		"title": "Blog",
 		"posts": posts,
+		"links": links,
 	}
 	err := templates.ExecuteTemplate(w, "blog_listing", data)
 	if err != nil {
@@ -115,6 +128,66 @@ func PostCtx(next http.Handler) http.Handler {
 	})
 }
 
+func postHandler(w http.ResponseWriter, r *http.Request) {
+	post := r.Context().Value("blog_post").(*Post)
+	data := map[string]interface{}{
+		"title": post.Title,
+		"post":  post,
+		"links": links,
+	}
+	err := templates.ExecuteTemplate(w, "blog_post", data)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+}
+
+func listProjects(w http.ResponseWriter, r *http.Request) {
+	data := map[string]interface{}{
+		"title":    "Projects",
+		"projects": projects,
+		"links":    links,
+	}
+	err := templates.ExecuteTemplate(w, "project_listing", data)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+}
+
+func ProjectCtx(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		projectId := chi.URLParam(r, "projectId")
+		var project *Project = nil
+		for _, p := range projects {
+			if strconv.Itoa(p.Id) == projectId {
+				project = &p
+				break
+			}
+		}
+		if project == nil {
+			http.Error(w, http.StatusText(404), 404)
+			return
+		}
+		ctx := context.WithValue(r.Context(), "project", project)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+func projectHandler(w http.ResponseWriter, r *http.Request) {
+	project := r.Context().Value("project").(*Project)
+	data := map[string]interface{}{
+		"title":   project.Title,
+		"project": project,
+		"links":   links,
+	}
+	err := templates.ExecuteTemplate(w, "project", data)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+}
+
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.RequestID)
@@ -131,20 +204,16 @@ func main() {
 		r.Get("/", listPosts)
 		r.Route("/{postId}", func(r chi.Router) {
 			r.Use(PostCtx)
-			r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-				post := r.Context().Value("blog_post").(*Post)
-				data := map[string]interface{}{
-					"title": post.Title,
-					"post":  post,
-				}
-				err := templates.ExecuteTemplate(w, "blog_post", data)
-				if err != nil {
-					log.Println(err.Error())
-					http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-				}
-			})
+			r.Get("/", postHandler)
 		})
+	})
 
+	r.Route("/projects", func(r chi.Router) {
+		r.Get("/", listProjects)
+		r.Route("/{projectId}", func(r chi.Router) {
+			r.Use(ProjectCtx)
+			r.Get("/", projectHandler)
+		})
 	})
 
 	log.Fatal(http.ListenAndServeTLS(":8080", "server.crt", "server.key", r))
